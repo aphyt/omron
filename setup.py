0a681967b8d8d4cf4c38c004053860d559b6c80c
Joe Ryan <jr@aphyt.com>
2023-08-19 20:06:53 -0700

    Fix package structure

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="omron",
-    version="0.1.2",
+    version="0.1.3",
     author="Joseph Ryan",
     author_email="jr@aphyt.com",
     description="A library to communicate with Omron devices",
